104e038d46dc64a3144f7df3ccbd0b33a12f18c1
lolmak <lolmakGS@ya.ru>
2025-02-18 01:47:29 +0700

    Развитие шутки

--- sosalTest/JS/buttons.js
+++ sosalTest/JS/buttons.js
@@ -2,6 +2,12 @@ const checkbox = document.getElementById('checkbox');
 const continueButton = document.getElementById('continueButton');
 const modal = document.getElementById('modal');
 const closeButton = document.querySelector('.close');
+const closeButtonYes = document.querySelector('.closeFooter');
+const NoSosalB = document.querySelector('.noFooter');
+const resultText = document.querySelector('.proof');
+const video = document.querySelector('.proofVideo');
+
+document.getElementById('proofVideoId').volume = 0.35;
 
 // Активация кнопки при нажатии на чекбокс
 checkbox.addEventListener('change', function() {
@@ -20,9 +26,22 @@ closeButton.addEventListener('click', function() {
     modal.style.display = 'none';
 });
 
+closeButtonYes.addEventListener('click', function() {
+    modal.style.display = 'none';
+});
+
 // Закрытие модального окна при клике вне его области
 window.addEventListener('click', function(event) {
-    if (event.target === modal) {
-        modal.style.display = 'none';
+	const resultText = document.querySelector('.result');
+	resultText.textContent = 'Вы победили!';
+});
+
+NoSosalB.addEventListener('click', function() {
+    resultText.style.display = 'block';
+	if (NoSosalB.textContent === 'ㅤㅤЯ ТОЧНО не сосал!') {
+        video.style.display = 'block';
+		NoSosalB.style.display = 'none';
+		closeButtonYes.textContent = 'Признаю, я сосал!';
     }
+	NoSosalB.textContent = 'ㅤㅤЯ ТОЧНО не сосал!';
 });
