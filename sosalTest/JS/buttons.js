2518bd5c9077be71bfb8ef195af34d71e5d9a289
lolmak <lolmakGS@ya.ru>
2025-02-18 00:46:39 +0700

    Добавление дипломов

--- sosalTest/JS/buttons.js
+++ sosalTest/JS/buttons.js
@@ -10,17 +10,19 @@ checkbox.addEventListener('change', function() {
 
 // Открытие модального окна при нажатии на кнопку
 continueButton.addEventListener('click', function() {
-    modal.style.display = 'block'; // Показываем модальное окно
+    modal.style.display = 'block';
+	checkbox.checked = false;
+	continueButton.disabled = !checkbox.checked;
 });
 
 // Закрытие модального окна при нажатии на крестик
 closeButton.addEventListener('click', function() {
-    modal.style.display = 'none'; // Скрываем модальное окно
+    modal.style.display = 'none';
 });
 
 // Закрытие модального окна при клике вне его области
 window.addEventListener('click', function(event) {
     if (event.target === modal) {
-        modal.style.display = 'none'; // Скрываем модальное окно
+        modal.style.display = 'none';
     }
 });
